5b492bc3bd94c33fea36090e2674f680a7811eeb
Git Test <ao152867@gmail.com>
2016-08-29 14:37:20 +0300

    Added a bit of interactivity to our program.

--- src/demoapp/DemoApp.java
+++ src/demoapp/DemoApp.java
@@ -5,6 +5,8 @@
  */
 package demoapp;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ao152867
@@ -15,8 +17,15 @@ public class DemoApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        
         // This will print Hello world! and add a newline.
         System.out.println("Hello world!");
+        // Asks the user for input.
+        System.out.println("Enter a number: ");
+        int n = reader.nextInt(); // Scans the next token of the input as an int.
+        System.out.println(n + " is a nice number but I prefer " + n*5/4+(n/3) + ".");
+        
     }
     
 }
