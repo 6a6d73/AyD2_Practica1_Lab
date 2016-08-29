af28eb15bf5511856a79017eca516854535af82c
Git Test <ao152867@gmail.com>
2016-08-29 13:29:22 +0300

    Hello world v1

---
+++ src/demoapp/DemoApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demoapp;
+
+/**
+ *
+ * @author ao152867
+ */
+public class DemoApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world!");
+    }
+    
+}
